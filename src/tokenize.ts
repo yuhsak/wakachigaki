2f8a098563bb8c41ebad4706c84b723f42819536
Yuhsak Inoue <yuhsak.inoue@gmail.com>
2021-11-15 15:35:07 +0900

    Fix invalid tokenization when ligatures are given

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -2,10 +2,11 @@ import { features } from './feature'
 import { predict } from './predict'
 
 export const tokenize = (text: string) => {
+  const chars = [...text]
   return predict(features(text))
     .reduce(
       (acc, willBreak, i) => {
-        acc[acc.length - 1] += text[i]
+        acc[acc.length - 1] += chars[i]
         if (willBreak) acc.push('')
         return acc
       },
